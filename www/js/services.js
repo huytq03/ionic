8091f31c5c45c2eb62589dadd2240a6986251a18
huytq03 <denta312@gmail.com>
2016-04-14 01:44:38 +0700

    finished left menu

--- www/js/services.js
+++ www/js/services.js
@@ -1,13 +1,17 @@
 angular.module('starter.services', [])
-.controller("Service",      function( $scope, $ionicModal, $timeout){
+.controller("Service",      function( $scope, $ionicModal, $timeout, $location){
         
         $scope.services = [
-          {"name" : "Delivery", "icon":"img/delivery.png","url":"/playlists"},
-         {"name" : "Pick up", "icon":"img/pickup.png","url":"/playlists"},
-         {"name" : "Catering", "icon":"img/catering.png","url":"/playlists"},
-         {"name" : "Dine in", "icon":"img/dine.png","url":"/playlists"}
+          {id: 1, "name" : "Delivery", "icon":"img/delivery.png","url":"/playlists", "icon_menu":"img/leftmenu/delivery.png"},
+         {id: 2,"name" : "Pick up", "icon":"img/pickup.png","url":"/playlists", "icon_menu":"img/leftmenu/pickup.png"},
+         {id: 3,"name" : "Catering", "icon":"img/catering.png","url":"/playlists", "icon_menu":"img/leftmenu/catering.png"},
+         {id: 4,"name" : "Dine in", "icon":"img/dine.png","url":"/playlists", "icon_menu":"img/leftmenu/dine.png"}
         ];
-        
+        $scope.changeService = function(serviceId) {
+
+        	//$location.path('/newValue');
+        }
+
         
         });
 
